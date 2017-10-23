49d7d98704491b2d4a1f6c665dab84bcaf340fda
Gary Abramson <gary.abramson@gmail.com>
2017-10-23 17:26:55 -0500

    implementing main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 """
 Module Docstring
 """
+import TSPFinder
 
 __author__ = "Your Name"
 __version__ = "0.1.0"
@@ -10,7 +11,17 @@ __license__ = "MIT"
 
 def main():
     """ Main entry point of the app """
-    print("hello world")
+    myfile = open("tsp.txt", "r")
+    vertices = int(myfile.readline())
+    tsp_finder = TSPFinder.TSPFinder(vertices)
+    points = [[0,0] for i in range(vertices)]
+    i = 0
+    for line in myfile:
+        values = line.split()
+        points[i][0] = float(values[0])
+        points[i][1] = float(values[1])
+        i += 1
+    points[0] = 1
 
 
 if __name__ == "__main__":
