fd53e096233c5175779103bfa70b1c1983f64592
Gary Abramson <gary.abramson@gmail.com>
2017-10-22 23:44:19 -0500

    Fixed pylint issues

--- TSPFinder.py
+++ TSPFinder.py
@@ -1,10 +1,16 @@
+""" Contains class TSPFinder """
+
 class TSPFinder:
+    """ find shortest tour in small graphs """
     def __init__(self, num_edges):
         self.distance = [[float("inf")] * num_edges for i in range(num_edges)]
 
     def add_edge(self, start, end, distance):
+        """ adds an edge to the graph """
         self.distance[start-1][end-1] = distance
         self.distance[end-1][start-1] = distance
 
     def get_shortest_tour(self):
+        """ computes the shortest tour """
         return 13
+    
--- test_TSP.py
+++ test_TSP.py
@@ -1,12 +1,14 @@
+""" Test TSFFinder """
 import TSPFinder
 
 def test_answer():
+    """ tests TSPFinder """
     tsp_finder = TSPFinder.TSPFinder(4)
-    tsp_finder.add_edge(1,2,2)
-    tsp_finder.add_edge(1,3,1)
-    tsp_finder.add_edge(1,4,4)
-    tsp_finder.add_edge(2,3,3)
-    tsp_finder.add_edge(2,4,5)
-    tsp_finder.add_edge(3,4,6)
+    tsp_finder.add_edge(1, 2, 2)
+    tsp_finder.add_edge(1, 3, 1)
+    tsp_finder.add_edge(1, 4, 4)
+    tsp_finder.add_edge(2, 3, 3)
+    tsp_finder.add_edge(2, 4, 5)
+    tsp_finder.add_edge(3, 4, 6)
     assert tsp_finder.get_shortest_tour() == 13
-
+    
